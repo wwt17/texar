e83ff4bfda9a0aeb82abbacbd3dbbf61c4968023
Wentao Wang <wwt10@pku.edu.cn>
2018-11-20 21:29:39 +0000

    refactor TransformerDecoder to support helper (test passed)

--- texar/modules/decoders/transformer_decoders.py
+++ texar/modules/decoders/transformer_decoders.py
@@ -25,7 +25,7 @@ from __future__ import print_function
 import collections
 
 import tensorflow as tf
-from tensorflow.python.util import nest
+from tensorflow.contrib.framework import nest
 
 from texar.core import layers
 from texar.module_base import ModuleBase
@@ -39,6 +39,7 @@ from texar.utils import beam_search
 from texar.utils.shapes import shape_list, mask_sequences
 from texar.utils import transformer_attentions as attn
 from texar.utils.mode import is_train_mode
+from texar.utils import transpose_batch_time
 
 
 __all__ = [
@@ -64,16 +65,16 @@ class TransformerDecoderOutput(
 class TransformerDecoder(ModuleBase):
     """Transformer decoder that applies multi-head attention for
     sequence decoding.
-    Stacked `~texar.modules.encoders.MultiheadAttentionEncoder` for
+    Stacked :class:`~texar.modules.encoders.MultiheadAttentionEncoder` for
     encoder-decoder attention and self attention,
-    `~texar.modules.FeedForwardNetwork` and residual connections.
+    :class:`~texar.modules.FeedForwardNetwork` and residual connections.
 
     Use the passed `embedding` variable as the parameters of the
     transform layer from output to logits.
 
     Args:
         embedding: A Tensor of shape `[vocab_size, dim]` containing the
-            word embeddng. The Tensor is used as the decoder output layer.
+            word embedding. The Tensor is used as the decoder output layer.
         hparams (dict or HParams, optional): Hyperparameters. Missing
             hyperparamerter will be set to default values. See
             :meth:`default_hparams` for the hyperparameter sturcture and
@@ -250,33 +251,28 @@ class TransformerDecoder(ModuleBase):
         token_emb = tf.nn.embedding_lookup(self._embedding, tokens)
         return token_emb
 
-    def _symbols_to_logits_fn(self, embedding_fn, max_length):
-        """Returns a function that accepts the decoded tokens and related
-        decoding status, and returns the logits of next token.
+    def _inputs_to_outputs_fn(self, max_length):
+        """Returns a function that accepts the inputs and related
+        decoding status, and returns the outputs of this step (for example,
+        the predicted logits of the next token).
         """
         channels = shape_list(self._embedding)[-1]
         timing_signal = self.position_embedder(max_length, channels)
 
-        def _impl(ids, step, cache):
+        def _impl(inputs, step, cache):
             """The function is called in dynamic decoding.
-
-            `ids` should be next_id of shape `[batch_size, decoded_lenth]`
-
-            Returned logits is of shape `[batch_size, 1]`
             """
-            ids = ids[:, -1:]
-            inputs = embedding_fn(ids)
             # Multiply embedding by sqrt of its dimention
             inputs *= self._embedding.shape.as_list()[-1]**0.5
-            inputs += timing_signal[:, step:step+1]
+            inputs += timing_signal[:, step]
             outputs = self._self_attention_stack(
-                inputs,
+                tf.expand_dims(inputs, axis=1),
                 memory=cache['memory'],
                 cache=cache,
             )
-            logits = self.output_layer(outputs)
-            logits = tf.squeeze(logits, axis=[1])
-            return logits, cache
+            outputs = self.output_layer(outputs)
+            outputs = tf.squeeze(outputs, axis=[1])
+            return outputs, cache
 
         return _impl
 
@@ -291,7 +287,9 @@ class TransformerDecoder(ModuleBase):
                alpha=0,
                start_tokens=None,
                end_token=None,
+               softmax_temperature=None,
                max_decoding_length=None,
+               helper=None,
                mode=None):
         """Performs decoding.
 
@@ -354,7 +352,7 @@ class TransformerDecoder(ModuleBase):
             alpha (float): Length penalty coefficient.
                 Refer to https://arxiv.org/abs/1609.08144
                 for more details.
-            tart_tokens (optional): An int Tensor of shape `[batch_size]`,
+            start_tokens (optional): An int Tensor of shape `[batch_size]`,
                 containing the start tokens.
                 Used when `decoding_strategy` = "infer_greedy" or
                 "infer_sample", or `beam_width` > 1.
@@ -362,10 +360,18 @@ class TransformerDecoder(ModuleBase):
                 of decoding.
                 Used when `decoding_strategy` = "infer_greedy" or
                 "infer_sample", or `beam_width` > 1.
+            softmax_temperature (optional): A float 0D Tensor, value to divide
+                the logits by before computing the softmax. Larger values
+                (above 1.0) result in more random samples. Must > 0. If `None`,
+                1.0 is used.
+                Used when `decoding_strategy="infer_sample"`.
             max_decoding_length (optional): An int scalar Tensor indicating
                 the maximum allowed number of decoding steps.
                 If `None` (default), use "max_decoding_length" defined in
                 :attr:`hparams`. Ignored in "train_greedy" decoding.
+            helper (optional): An instance of
+                :tf_main:`Helper <contrib/seq2seq/Helper>` that defines the
+                decoding strategy. If given, `decoding_strategy` is ignored.
             mode (optional): A tensor taking value in
                 :tf_main:`tf.estimator.ModeKeys <estimator/ModeKeys>`, including
                 `TRAIN`, `EVAL`, and `PREDICT`. Controls dropout mode.
@@ -438,29 +444,43 @@ class TransformerDecoder(ModuleBase):
             )
             rets = output
 
-        else: # Inference decoding
+        else: # Inference-like decoding
 
             if max_decoding_length is None:
                 max_decoding_length = self._hparams.max_decoding_length
 
             if beam_width <= 1:
-                logits, preds, sequence_length = self._infer_decoding(
-                    self._prepare_tokens_to_embeds,
-                    start_tokens,
-                    end_token,
+                # Prepare helper
+                if helper is not None:
+                    pass
+                elif decoding_strategy is not None:
+                    if decoding_strategy == "infer_greedy":
+                        helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
+                            self._embedding, start_tokens, end_token)
+                    elif decoding_strategy == "infer_sample":
+                        helper = tf.contrib.seq2seq.SampleEmbeddingHelper(
+                            self._embedding, start_tokens, end_token,
+                            softmax_temperature)
+                    else:
+                        raise ValueError(
+                            "Unknown decoding strategy: {}".format(
+                                decoding_strategy))
+                else:
+                    raise ValueError(
+                        "Must provide one of 'decoding_strategy' and 'helper'."
+                        " We may implement helper hparams in the future.")
+                self._helper = helper
+
+                rets = self._infer_decoding(
                     decode_length=max_decoding_length,
                     memory=memory,
                     memory_attention_bias=memory_attention_bias,
                     decoding_strategy=decoding_strategy,
                 )
-                output = TransformerDecoderOutput(
-                    logits=logits,
-                    sample_id=preds)
-                rets = output, sequence_length
+
             else:
                 # The output format is different when running beam search
                 sample_id, log_prob = self._beam_decode(
-                    self._prepare_tokens_to_embeds,
                     start_tokens,
                     end_token,
                     beam_width=beam_width,
@@ -585,41 +605,56 @@ class TransformerDecoder(ModuleBase):
         return cache
 
     def _infer_decoding(self,
-                        embedding_fn,
-                        start_tokens,
-                        end_token,
                         decode_length,
                         memory,
                         memory_attention_bias,
                         decoding_strategy):
-        """Performs "infer_greedy" or "infer_sample" decoding.
+        """Performs inference-like (such as "infer_greedy" or "infer_sample")
+        decoding.
         """
-        batch_size = tf.shape(start_tokens)[0]
-        finished = tf.fill([batch_size], False)
-        seq_length = tf.zeros([batch_size], dtype=tf.int32)
+        batch_size = self._helper.batch_size
+        output_size = TransformerDecoderOutput(
+            logits=tf.TensorShape([self._vocab_size]),
+            sample_id=self._helper.sample_ids_shape)
+        output_dtype = TransformerDecoderOutput(
+            logits=tf.float32,
+            sample_id=self._helper.sample_ids_dtype)
+
+        def _shape(batch_size, from_shape):
+            if (not isinstance(from_shape, tf.TensorShape) or
+                    from_shape.ndims == 0):
+                return tf.TensorShape(None)
+            else:
+                batch_size = tf.contrib.util.constant_value(
+                    tf.convert_to_tensor(
+                        batch_size, name="batch_size"))
+                return tf.TensorShape([batch_size]).concatenate(from_shape)
+
+        def _create_ta(s, d):
+            return tf.TensorArray(
+                dtype=d,
+                size=0,
+                dynamic_size=True,
+                element_shape=_shape(batch_size, s))
+
+        finished, inputs = self._helper.initialize()
+        seq_length = tf.zeros_like(finished, dtype=tf.int32)
         step = tf.constant(0)
-        decoded_ids = tf.zeros([batch_size, 0], dtype=tf.int32)
-        logits_list = tf.zeros([batch_size, 0, self._vocab_size],
-                               dtype=tf.float32)
-        next_id = tf.expand_dims(start_tokens, 1)
+        outputs_ta = nest.map_structure(_create_ta, output_size, output_dtype)
 
         cache = self._init_cache(memory, memory_attention_bias)
-        symbols_to_logits_fn = self._symbols_to_logits_fn(
-            embedding_fn,
-            max_length=decode_length+1
-        )
+        inputs_to_outputs_fn = self._inputs_to_outputs_fn(
+            max_length=decode_length+1)
 
-        def _body(step, finished, next_id, decoded_ids, cache, logits_list,
-                  seq_length):
-            logits, cache = symbols_to_logits_fn(next_id, step, cache)
-
-            if decoding_strategy == 'infer_greedy':
-                next_id = tf.argmax(logits, -1, output_type=tf.int32)
-            elif decoding_strategy == 'infer_sample':
-                sample_id_sampler = tf.distributions.Categorical(logits=logits)
-                next_id = sample_id_sampler.sample()
-
-            cur_finished = tf.equal(next_id, end_token)
+        def _body(step, finished, inputs, cache, outputs_ta, seq_length):
+            outputs, cache = inputs_to_outputs_fn(inputs, step, cache)
+            sample_ids = self._helper.sample(
+                time=step, outputs=outputs, state=None)
+            cur_finished, next_inputs, _ = self._helper.next_inputs(
+                time=step,
+                outputs=outputs,
+                state=None,
+                sample_ids=sample_ids)
 
             update_len = tf.logical_and(
                 tf.logical_not(finished),
@@ -629,42 +664,41 @@ class TransformerDecoder(ModuleBase):
                 tf.fill(tf.shape(seq_length), step+1),
                 seq_length)
 
-            next_id = tf.expand_dims(next_id, axis=1)
-
             finished |= cur_finished
 
-            # Keep the shape as [batch_size, seq_len]
-            logits = tf.expand_dims(logits, axis=1)
-            logits_list = tf.concat([logits_list, logits], axis=1)
-            decoded_ids = tf.concat([decoded_ids, next_id], axis=1)
+            outputs_ta = nest.map_structure(
+                lambda ta, out: ta.write(step, out),
+                outputs_ta,
+                TransformerDecoderOutput(
+                    logits=outputs,
+                    sample_id=sample_ids))
 
-            return step+1, finished, next_id, decoded_ids, cache, \
-                    logits_list, seq_length
+            return step+1, finished, next_inputs, cache, outputs_ta, seq_length
 
         def _not_finished(i, finished, *_):
             return (i < decode_length) & tf.logical_not(tf.reduce_all(finished))
 
-        _, _, _, decoded_ids, _, logits_list, seq_length = tf.while_loop(
+        _, _, _, _, outputs_ta, seq_length = tf.while_loop(
             _not_finished,
             _body,
-            loop_vars=(step, finished, next_id, decoded_ids, cache, logits_list,
-                       seq_length),
+            loop_vars=(step, finished, inputs, cache, outputs_ta, seq_length),
             shape_invariants=(
                 tf.TensorShape([]),
                 tf.TensorShape([None]),
                 tf.TensorShape([None, None]),
-                tf.TensorShape([None, None]),
                 nest.map_structure(beam_search.get_state_shape_invariants,
                                    cache),
-                tf.TensorShape([None, None, None]),
+                nest.map_structure(lambda ta: tf.TensorShape(None), outputs_ta),
                 tf.TensorShape([None])
-                )
             )
+        )
 
-        return logits_list, decoded_ids, seq_length
+        final_outputs = nest.map_structure(lambda ta: ta.stack(), outputs_ta)
+        final_outputs = transpose_batch_time(final_outputs)
+
+        return final_outputs, seq_length
 
     def _beam_decode(self,
-                     embedding_fn,
                      start_tokens,
                      end_token,
                      memory,
@@ -673,8 +707,11 @@ class TransformerDecoder(ModuleBase):
                      beam_width=5,
                      alpha=0.6):
         cache = self._init_cache(memory, memory_attention_bias)
-        symbols_to_logits_fn = self._symbols_to_logits_fn(embedding_fn, \
-            max_length=decode_length+1)
+        inputs_to_outputs_fn = self._inputs_to_outputs_fn(decode_length+1)
+        def symbols_to_logits_fn(ids, step, cache):
+            return inputs_to_outputs_fn(
+                self._prepare_tokens_to_embeds(ids[:, -1]), step, cache)
+
         outputs, log_prob = beam_search.beam_search(
             symbols_to_logits_fn,
             start_tokens,
