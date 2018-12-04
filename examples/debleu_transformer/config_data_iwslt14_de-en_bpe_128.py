062308a21a29741d6a5751d85745707563f2661c
Wentao Wang <wwt10@pku.edu.cn>
2018-12-04 00:08:19 -0500

    add more msp configs

---
+++ examples/debleu_transformer/config_data_iwslt14_de-en_bpe_128.py
@@ -0,0 +1,6 @@
+n_tokens = 128
+test_batch_size = 64
+
+filename_prefix = "processed."
+input_dir = 'temp/run_de_en_bpe/data'
+vocab_file = input_dir + '/processed.vocab.pickle'
---
+++ examples/debleu_transformer/config_train_pg_msp5_200.py
@@ -0,0 +1,101 @@
+max_epochs = 1000
+steps_per_val = 2000
+steps_per_test = 10000
+
+threshold_steps = 20000
+minimum_interval_steps = 10000
+phases = [
+    # (config_data, config_train, mask_pattern)
+    ("train", "xe", None),
+    ("train", "pg_msp", None),
+]
+
+max_order = 4
+weights = [.1, .3, .3, .3]
+
+loss_label_confidence = 0.9
+tau = 1.
+
+infer_max_decoding_length = 200
+infer_configs = [
+    # (max_decoding_length, beam_width, alpha)
+    (infer_max_decoding_length, 1, 0.6),
+    (infer_max_decoding_length, 5, 0.6),
+    (infer_max_decoding_length, 10, 0.6),
+]
+
+n_samples = 5
+sample_max_decoding_length = 200
+greedy_max_decoding_length = 200
+
+weight_pg_grd = 1.
+weight_pg_msp = 1.
+
+train_xe = {
+    'name': 'xe',
+    'optimizer': {
+        'type': 'AdamOptimizer',
+        'kwargs': {
+            'beta1': 0.9,
+            'beta2': 0.997,
+            'epsilon': 1e-9
+        }
+    },
+}
+
+train_debleu = {
+    'name': 'debleu',
+    'optimizer': {
+        'type': 'GradientDescentOptimizer',
+        'kwargs': {
+            'learning_rate': 1e-4
+        }
+    },
+    'gradient_clip': {
+        'type': 'clip_by_global_norm',
+        'kwargs': {
+            'clip_norm': 25.
+        }
+    },
+}
+
+train_pg_grd = {
+    'name': 'pg_grd',
+    'optimizer': {
+        'type': 'GradientDescentOptimizer',
+        'kwargs': {
+            'learning_rate': 1e-4
+        }
+    },
+    'gradient_clip': {
+        'type': 'clip_by_global_norm',
+        'kwargs': {
+            'clip_norm': 25.
+        }
+    },
+}
+
+train_pg_msp = {
+    'name': 'pg_msp',
+    'optimizer': {
+        'type': 'GradientDescentOptimizer',
+        'kwargs': {
+            'learning_rate': 1e-4
+        }
+    },
+    'gradient_clip': {
+        'type': 'clip_by_global_norm',
+        'kwargs': {
+            'clip_norm': 25.
+        }
+    },
+}
+
+d_model = 512
+
+lr = {
+    'learning_rate_schedule': 'constant.linear_warmup.rsqrt_decay.rsqrt_depth',
+    'lr_constant': 2 * (d_model ** -0.5),
+    'static_lr': 1e-3,
+    'warmup_steps': 16000,
+}
