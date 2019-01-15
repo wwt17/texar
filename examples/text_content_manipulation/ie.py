db09b79c20cf526ee79e4593c68d951621c4b7e1
Wentao Wang <wwt10@pku.edu.cn>
2019-01-15 00:05:40 -0500

    add bleu_only config

--- examples/text_content_manipulation/ie.py
+++ examples/text_content_manipulation/ie.py
@@ -37,39 +37,49 @@ def get_step_number(filename, verbose=False):
         return 0
 
 
-def get_precrec(
-        gold_file, hypo_file, inter_file, ref_file, gpuid=0,
-        dict_pfx=os.path.join(data2text_dir, "roto-ie"),
-        json_path=os.path.join(data2text_dir, "rotowire"),
-        write_record=True):
-    rough_res = prep_generated_data(
-        hypo_file, dict_pfx, inter_file,
-        trdata=get_json_dataset(json_path, "train"),
-        val_file=gold_file)
-
-    ret = subprocess.call(
-        ["th", "extractor.lua",
-         "-gpuid", str(gpuid+1),
-         "-datafile", os.path.abspath("{}.h5".format(dict_pfx)),
-         "-dict_pfx", os.path.abspath(dict_pfx),
-         "-just_eval",
-         "-preddata", os.path.abspath(inter_file)],
-        cwd=data2text_dir)
-    if ret != 0:
-        raise Exception(
-            "run extractor.lua failed with return value {}".format(ret))
-
-    pred_file = "{}-tuples.txt".format(inter_file)
-    gold_items, pred_items = map(get_items, (gold_file, pred_file))
-    with open("{}.res.txt".format(hypo_file), "w") as itemwise_outfile:
-        precrec = calc_precrec(gold_items, pred_items,
-                               itemwise_outfile=itemwise_outfile)
-
+def get_bleu(ref_file, hypo_file):
     refs = read_sents_from_file(ref_file)
     refs = [[ref] for ref in refs]
     hypos = read_sents_from_file(hypo_file)
     bleu = corpus_bleu(refs, hypos) * 1e-2
     print("BLEU: {:.2%}".format(bleu))
+    return bleu
+
+
+def get_precrec(
+        gold_file, hypo_file, inter_file, ref_file, bleu_only=False, gpuid=0,
+        dict_pfx=os.path.join(data2text_dir, "roto-ie"),
+        json_path=os.path.join(data2text_dir, "rotowire"),
+        write_record=True):
+    if not bleu_only:
+        rough_res = prep_generated_data(
+            hypo_file, dict_pfx, inter_file,
+            trdata=get_json_dataset(json_path, "train"),
+            val_file=gold_file)
+
+        ret = subprocess.call(
+            ["th", "extractor.lua",
+             "-gpuid", str(gpuid+1),
+             "-datafile", os.path.abspath("{}.h5".format(dict_pfx)),
+             "-dict_pfx", os.path.abspath(dict_pfx),
+             "-just_eval",
+             "-preddata", os.path.abspath(inter_file)],
+            cwd=data2text_dir)
+        if ret != 0:
+            raise Exception(
+                "run extractor.lua failed with return value {}".format(ret))
+
+        pred_file = "{}-tuples.txt".format(inter_file)
+        gold_items, pred_items = map(get_items, (gold_file, pred_file))
+        with open("{}.res.txt".format(hypo_file), "w") as itemwise_outfile:
+            precrec = calc_precrec(gold_items, pred_items,
+                                   itemwise_outfile=itemwise_outfile)
+
+    else:
+        rough_res = 0., 0.
+        precrec = 0., 0.
+
+    bleu = get_bleu(ref_file, hypo_file)
 
     if write_record:
         dirname, basename = os.path.split(hypo_file)
@@ -87,6 +97,7 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--gold_file", default=os.path.join("nba_data", "gold.test.txt"))
     argparser.add_argument("--ref_file", default=os.path.join("nba_data", "nba.sent_ref.test.txt"))
+    argparser.add_argument("--bleu_only", action="store_true")
     argparser.add_argument("hypo_files", nargs="+")
     argparser.add_argument("--inter_file", default="")
     argparser.add_argument("--gpuid", type=int, default=0)
@@ -101,4 +112,4 @@ if __name__ == "__main__":
                 s = s[:-len(suffix)]
             args.inter_file = s + ".h5"
         prec, rec = get_precrec(
-            args.gold_file, hypo_file, args.inter_file, args.ref_file, gpuid=args.gpuid)
+            args.gold_file, hypo_file, args.inter_file, args.ref_file, bleu_only=args.bleu_only, gpuid=args.gpuid)
