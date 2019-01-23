9f0cadf503fa7d8945333467369333aef6be1d68
Wentao Wang <wwt10@pku.edu.cn>
2019-01-23 14:53:41 -0500

    refine makefile

--- examples/text_content_manipulation/nba_data/make_vocabs.py
+++ examples/text_content_manipulation/nba_data/make_vocabs.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from texar.data import make_vocab as make_vocab
+from texar.data import make_vocab
 
 try:
     import codecs
